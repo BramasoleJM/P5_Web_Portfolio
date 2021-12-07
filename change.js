fe03711dc84f4c86bbac0dfae0ec85f6c6cdf014
BramasoleJM <bramasolejm030206@gmail.com>
2021-12-06 22:37:49 -0500

    first_draft

---
+++ change.js
@@ -0,0 +1,7 @@
+function change_text() {
+    document.getElementById("post").innerHTML = "Long Live P5";
+}
+
+function change_color() {
+    document.getElementById("post").style.color = "#e99dd6";
+}
